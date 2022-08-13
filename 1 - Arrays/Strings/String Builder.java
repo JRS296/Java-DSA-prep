a2c263b87bc9a9426b148b2bec260211d8409b11
JRS296 <jonathansamuel296@gmail.com>
2022-08-13 22:24:01 +0530

    Commit #9 - Modified 2 file(s) - 13.08.2022 @22:23

---
+++ 1 - Arrays/Strings/String Builder.java
@@ -0,0 +1,17 @@
+
+class StringBuilder_exp {
+    public static void main(String[] args) {
+        int n = 10000;
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            str.append("hello");
+        }
+        String s = str.toString();
+    }
+}
+
+//If you have to concatenate strings often,
+// it will be better to use some other data structures 
+//like StringBuilder. The below code runs in O(n) complexity.
+
+//Normal methods to concatenate is O(n^2), so beware
