4c671d905de9a741fd2ff6a351861ff5599e599e
JRS296 <jonathansamuel296@gmail.com>
2022-10-18 23:43:48 +0530

    Commit #28 - Modified 2 file(s) - 18.10.2022 @23:43

--- 1 - Arrays/2D Arrays/1.0 - Basics + Single For Loop Traversal.java
+++ 1 - Arrays/2D Arrays/1.0 - Basics + Single For Loop Traversal.java
@@ -1,36 +1,25 @@
 class GFG {
     public static void print2DMatrix(
-        int arr[][], int rows, int columns)
-    {
- 
-        // Iterate over the range
-        // [0, rows*columns]
-        for (int i = 0;
-             i < rows * columns; i++) {
- 
+            int arr[][], int rows, int columns) {
+        // Iterate over the range [0, rows*columns]
+        for (int i = 0; i < rows * columns; i++) {
             // Find row and column index
             int row = i / columns;
             int col = i % columns;
- 
-            // Print the element at
-            // current index
-            System.out.print(
-                arr[row][col] + " ");
+            System.out.print(arr[row][col] + " ");
         }
     }
- 
-    // Driver Code
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         // Given matrix mat[][]
         int[][] mat = { { 1, 2, 3 },
-                        { 4, 5, 6 },
-                        { 7, 8, 9 } };
- 
+                { 4, 5, 6 },
+                { 7, 8, 9 } };
+
         // Dimensions of the matrix
         int N = mat.length;
         int M = mat[0].length;
- 
+
         // Function Call
         print2DMatrix(mat, N, M);
     }
