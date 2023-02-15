a94e008c48600c2c278af2b79040989e22a4d51d
JRS296 <jonathansamuel296@gmail.com>
2023-02-15 18:29:30 +0530

    Commit #52 - Modified 8 file(s) - 15.02.2023 @18:29

--- 11 - Algorithmic Paradigm/Union Find - Disjoint Sets/Union Find.java
+++ 11 - Algorithmic Paradigm/Union Find - Disjoint Sets/Union Find.java
@@ -1,4 +1,4 @@
-public class UnionFind {
+class UnionFind {
     private int[] parent;
     public UnionFind(int n) {
         parent = new int[n];
@@ -63,5 +63,31 @@ public class Main {
         System.out.println(uf.Find(3));
         uf.Union(3, 4);
         System.out.println(uf.Find(3)); // after join, 3's parent is 4.
+    }
+}
+
+//Eg: (Lexographically smallest num)
+class Solution { //Same approach as planned, but since lexigraphically highest character is favoured, that is considered within array
+    int[] parent;
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        parent = new int[26];
+        for(int i=0;i<26;i++)
+            parent[i]=i;
+        for(int i=0;i<s1.length();i++){
+            int u = find(s1.charAt(i)-'a');
+            int v = find(s2.charAt(i)-'a');
+            if(u<v)
+              parent[v]=u;
+            else if(u>v)
+              parent[u]=v;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<baseStr.length();i++)
+            sb.append((char)(find(baseStr.charAt(i)-'a')+'a'));
+        return sb.toString();
+    }
+
+    private int find(int u){
+        return parent[u]==u? u:find(parent[u]);
     }
 }
---
+++ 2 - Searching Algorithms/Quick Select Search/Kth Largest - Quick Select.java
@@ -0,0 +1,90 @@
+class Solution {
+    //quickselect: the average of time:o(n), it depends on the pivot we choose.
+//space:o(logn)
+public int findKthLargest(int[] nums, int k) {
+        return quickselect(nums, 0, nums.length - 1, k);
+    }
+public int quickselect(int[] nums, int start, int end, int k){
+        if(start >= end) return nums[start];
+        int i = start;
+        int j = end;
+        int pivot = nums[(end - start) / 2 + start];
+        while(i <= j){
+            while(i <= j && nums[i] > pivot){//Only Change for smallest/largest
+                i++;
+            }
+            while(i <= j && nums[j] < pivot){//Only Change for smallest/largest
+                j--;
+            }
+            if(i <= j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        //start,...,j,i,...,end
+        if(start + k - 1 <= j){
+            return quickselect(nums, start, j, k);
+        }
+        if(start + k - 1 >= i){
+            return quickselect(nums, i, end, k - i + start);
+        }
+        return nums[j + 1];
+    }
+}
+
+/*
+Runtime
+5 ms
+Beats
+97.20%
+Memory
+49.8 MB
+Beats
+90.33%
+ */
+
+//Based off Quick Sort:
+class Solution2 {
+    //quicksort: time:o(nlogn) space:o(logn)
+public int findKthLargest(int[] nums, int k) {
+        quicksort(nums, 0, nums.length - 1, k);
+        return nums[k - 1];
+    }
+    public void quicksort(int[] nums, int start, int end, int k){
+        if(start >= end) return;
+        int i = start;
+        int j = end;
+        int pivot = nums[(end - start) / 2 + start];
+        while(i <= j){
+            while(i <= j && nums[i] > pivot){
+                i++;
+            }
+            while(i <= j && nums[j] < pivot){
+                j--;
+            }
+            if(i <= j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        quicksort(nums, start, j, k);
+        quicksort(nums, i, end, k);
+    }
+}
+
+/*
+Runtime
+36 ms
+Beats
+54.99%
+Memory
+49.6 MB
+Beats
+97.78%
+ */
